b490c2dbc20f21ff2d89e3a64f188c02cb4d1ba5
nbushehri <nader.boushehri@gmail.com>
2022-07-20 09:13:15 -0600

    update lit converter script

--- tests/arm-tv/convert-to-lit.sh
+++ tests/arm-tv/convert-to-lit.sh
@@ -5,5 +5,5 @@ FILES=`ls -d -1 $1*.ll`
 for f in $FILES; do
   out="${f%.ll}.srctgt.ll"
   mv "$f" "$out"
-  sed -i '1s/^/; TEST-ARGS: -backend-tv --disable-undef-input\n\n/' "$out"
+  sed -i '1s/^/; TEST-ARGS: -backend-tv --disable-undef-input --disable-poison-input\n\n/' "$out"
 done
