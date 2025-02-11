031f02fe21bfa2925a8de6f33900ba176ee47db9
John Regehr <regehr@cs.utah.edu>
2025-02-11 13:19:48 -0700

    better fix

--- backend_tv/lifter.cpp
+++ backend_tv/lifter.cpp
@@ -3892,6 +3892,7 @@ class arm2llvm : public aslp::lifter_interface_llvm {
            (baseReg == AArch64::SP) || (baseReg == AArch64::LR) ||
            (baseReg == AArch64::FP) || (baseReg == AArch64::XZR));
     assert((offsetReg >= AArch64::X0 && offsetReg <= AArch64::X28) ||
+	   (offsetReg == AArch64::LR) ||
            (offsetReg == AArch64::FP) || (offsetReg == AArch64::SP) ||
            (offsetReg == AArch64::XZR) ||
            (offsetReg >= AArch64::W0 && offsetReg <= AArch64::W30) ||
