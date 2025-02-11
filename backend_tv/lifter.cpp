8ff7b45b18fa9a759d454c517971bc9717821b11
John Regehr <regehr@cs.utah.edu>
2025-02-11 12:31:32 -0700

    oops, assertion was too tight

--- backend_tv/lifter.cpp
+++ backend_tv/lifter.cpp
@@ -4375,7 +4375,7 @@ class arm2llvm : public aslp::lifter_interface_llvm {
            (baseReg == AArch64::FP) || (baseReg == AArch64::XZR));
     assert((offsetReg >= AArch64::X0 && offsetReg <= AArch64::X28) ||
            (offsetReg == AArch64::FP) || (offsetReg == AArch64::XZR) ||
-           (offsetReg >= AArch64::W0 && offsetReg <= AArch64::W28) ||
+           (offsetReg >= AArch64::W0 && offsetReg <= AArch64::W30) ||
            (offsetReg == AArch64::WZR));
 
     int extTyp, shiftAmt;
